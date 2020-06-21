c06e36fd879f9234109339ca8faaf2269b3e4214
Sergey Deshko <axilhik@gmail.com>
2020-06-21 03:53:31 +0300

    Add task 2 - checkMultiplicity

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ if (userAgeToNumber >= 18) {
 } else {
     alert('you are a minor');
 }
+
+// checkMultiplicity
+
+const number1 = prompt ('Pleas enter the first number ');
+
+const number1ToNumber = number1 -0;
+
+const number2 = prompt ('Pleas enter the second number ');
+
+const number2ToNumber = number2 -0;
+
+if (number1ToNumber % number2ToNumber) {
+    alert('numbers are not multiple')
+} else {
+    alert('numbers are multiple')
+}
