da143938b7a169cf7501ae58fce83ca6c059e36f
Sergey Deshko <axilhik@gmail.com>
2020-07-06 18:32:35 +0300

    add JS
    Refactoring tasks

--- index.js
+++ index.js
@@ -2,57 +2,25 @@
 
 isAdult();
 
-function isAdult() {
+function isAdult(age) {
 
-    const userAgeInput = prompt('Pleas enter your age ');
-
-    const userAgeToNumber = userAgeInput - 0;
-
-    if (userAgeToNumber >= 18) {
-        alert('you are an adult');
-    } else {
-        alert('you are a minor');
-    }
+    return age >= 18;
 }
 
 // checkMultiplicity
 
 checkMultiplicity();
 
-function checkMultiplicity() {
+function checkMultiplicity(number1, number2,) {
 
-
-    const number1 = prompt('Pleas enter the first number ');
-
-    const number1ToNumber = number1 - 0;
-
-    const number2 = prompt('Pleas enter the second number ');
-
-    const number2ToNumber = number2 - 0;
-
-    if (number1ToNumber % number2ToNumber) {
-        alert('numbers are not multiple')
-    } else {
-        alert('numbers are multiple')
-    }
+    return !(number1 % number2);
 }
 //trigon
 
 trigon();
 
-function trigon() {
+function trigon(a, b, c, ) {
 
-    const a = prompt('enter side a ');
-    const b = prompt('enter side b ');
-    const c = prompt('enter side c ');
-    const aToNumber = a - 0;
-    const bToNumber = b - 0;
-    const cToNumber = c - 0;
-
-    if (aToNumber < bToNumber + cToNumber && bToNumber < aToNumber + cToNumber && cToNumber < aToNumber + bToNumber) {
-        alert('a triangle is possible to build');
-    } else {
-        alert('a triangle is not possible to build');
-    }
+    return a < (b + c) && b < (a + c) && c < (a + b);
 }
 
