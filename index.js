c9ac2bfcafd8ad37c8e68e2be27908ed4e9c11e5
Sergey Deshko <axilhik@gmail.com>
2020-07-06 18:04:21 +0300

    add JS
    Refactoring tasks

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
 //isAdult
 
-const userAgeInput = prompt ('Pleas enter your age ');
+isAdult();
 
-const userAgeToNumber = userAgeInput - 0;
+function isAdult() {
 
-if (userAgeToNumber >= 18) {
-    alert('you are an adult');
-} else {
-    alert('you are a minor');
+    const userAgeInput = prompt('Pleas enter your age ');
+
+    const userAgeToNumber = userAgeInput - 0;
+
+    if (userAgeToNumber >= 18) {
+        alert('you are an adult');
+    } else {
+        alert('you are a minor');
+    }
 }
 
 // checkMultiplicity
 
-const number1 = prompt ('Pleas enter the first number ');
+checkMultiplicity();
 
-const number1ToNumber = number1 -0;
+function checkMultiplicity() {
 
-const number2 = prompt ('Pleas enter the second number ');
 
-const number2ToNumber = number2 -0;
+    const number1 = prompt('Pleas enter the first number ');
 
-if (number1ToNumber % number2ToNumber) {
-    alert('numbers are not multiple')
-} else {
-    alert('numbers are multiple')
+    const number1ToNumber = number1 - 0;
+
+    const number2 = prompt('Pleas enter the second number ');
+
+    const number2ToNumber = number2 - 0;
+
+    if (number1ToNumber % number2ToNumber) {
+        alert('numbers are not multiple')
+    } else {
+        alert('numbers are multiple')
+    }
 }
+//trigon
 
-//Треугольник
+trigon();
 
-const a = prompt ('enter side a ');
-const b = prompt ('enter side b ');
-const c = prompt ('enter side c ');
-const aToNumber = a -0;
-const bToNumber = b -0;
-const cToNumber = c -0;
+function trigon() {
 
-if (aToNumber < bToNumber + cToNumber && bToNumber < aToNumber + cToNumber && cToNumber < aToNumber + bToNumber) {
-    alert('a triangle is possible to build');
-} else {
-    alert('a triangle is not possible to build');
+    const a = prompt('enter side a ');
+    const b = prompt('enter side b ');
+    const c = prompt('enter side c ');
+    const aToNumber = a - 0;
+    const bToNumber = b - 0;
+    const cToNumber = c - 0;
+
+    if (aToNumber < bToNumber + cToNumber && bToNumber < aToNumber + cToNumber && cToNumber < aToNumber + bToNumber) {
+        alert('a triangle is possible to build');
+    } else {
+        alert('a triangle is not possible to build');
+    }
 }
 
